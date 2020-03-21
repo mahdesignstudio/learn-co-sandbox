8f3c9f06a8a7c5831123c19e4be529dc152af9e9
Melissa Hernandez <hdez.melissaa@gmail.com>
2020-03-21 02:58:23 +0000

    update notes

---
+++ learning-case-statements.rb
@@ -0,0 +1,11 @@
+current_weather = "sunny"
+
+case current_weather
+when "sunny"
+  puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+    when "snowing"
+      puts "bundle up"
+    end
+    
